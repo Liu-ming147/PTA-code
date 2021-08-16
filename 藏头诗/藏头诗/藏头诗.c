ab6eeda88f24329c40579ace84603d2c93117c04
Liu-ming147 <1479763099@qq.com>
2021-08-16 16:52:36 +0800

    提交代码

---
+++ 藏头诗/藏头诗/藏头诗.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	char a[4][15], b[9];
+	int i, j = 0;
+	for (i = 0; i < 4; i++)
+		scanf("%s", a[i]);
+	for (i = 0; i < 4; i++)
+	{
+		b[j++] = a[i][0];
+		b[j++] = a[i][1];
+	}
+	b[j] = '\0';
+	printf("%s\n", b);
+
+	return 0;
+}
